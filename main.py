86d651759b4a914308abafc66a253774bb839aa7
Ebenezer <66065626+sojjyCodes@users.noreply.github.com>
2020-07-03 19:57:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome() #for firefox you can use webdriver.Firefox()
+driver = webdriver.Chrome()
 
 
 usr = input('Enter Email Id: ')
